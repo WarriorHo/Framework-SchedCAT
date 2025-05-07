c9116f5b135bc47b8fbef74d1825a531e99017bb
hufanhang <93808184+WarriorHo@users.noreply.github.com>
2025-05-07 20:00:15 +0800

    Add util.py new version add partition

--- schedcat-experiments-master/rta/util.py
+++ schedcat-experiments-master/rta/util.py
@@ -54,44 +54,105 @@ def generate_task_set(conf):
     
     return ts
 
+# def rta_test(taskset, oh, conf, include_consumers=False):
+#     ts = taskset
+#     if not include_consumers:
+#         ts = TaskSystem([t for t in ts if not t.is_consumer])
+#     framework = get_framework('rta')
+#     for t in ts:
+#         t.cost = framework.calculate_execution_time(t)
+#     sched = int(bound_response_times(conf.num_cpus, ts))
+#     return (sched, 0)
+
+# def rta_omnilog_test(taskset, oh, conf, include_consumers=False):
+#     ts = taskset
+#     if not include_consumers:
+#         ts = TaskSystem([t for t in ts if not t.is_consumer])
+#     min_period_task = min(ts, key=lambda t: t.period)
+#     q_sigma = min_period_task.period
+    
+#     consumer = SporadicTask(0, q_sigma, q_sigma)
+#     consumer.is_consumer = True
+#     consumer.preemption_level = -1
+#     consumer.partition = 0
+#     ts.append(consumer)
+    
+#     framework = get_framework('omnilog')
+#     for t in ts:
+#         t.cost = framework.calculate_execution_time(t)
+#     sched = int(bound_response_times_omnilog(conf.num_cpus, ts, framework.delta, framework.beta))
+#     return (sched, 0)
+
+# def rta_nodrop_test(taskset, oh, conf, include_consumers=True):
+#     ts = taskset
+#     if not include_consumers:
+#         ts = TaskSystem([t for t in ts if not t.is_consumer])
+#     framework = get_framework('nodrop')
+#     for t in ts:
+#         t.cost = framework.calculate_execution_time(t)
+#     sched = int(bound_response_times_nodrop(ts, framework.delta, framework.alpha, framework.beta))
+#     return (sched, 0)
+
+def iter_partitions_ts(taskset):
+    partitions = {}
+    for t in taskset:
+        if t.partition not in partitions:
+            partitions[t.partition] = []
+        partitions[t.partition].append(t)
+    for p in partitions.itervalues():
+        yield TaskSystem(p)
+
 def rta_test(taskset, oh, conf, include_consumers=False):
-    ts = taskset
+    ts = TaskSystem(taskset)
     if not include_consumers:
         ts = TaskSystem([t for t in ts if not t.is_consumer])
     framework = get_framework('rta')
     for t in ts:
         t.cost = framework.calculate_execution_time(t)
-    sched = int(bound_response_times(conf.num_cpus, ts))
-    return (sched, 0)
+        
+    for partition in iter_partitions_ts(ts):
+        if not bound_response_times(1, partition):
+            return (0, 0)
+    return (1, 0)
 
 def rta_omnilog_test(taskset, oh, conf, include_consumers=False):
-    ts = taskset
+    ts = TaskSystem(taskset)
     if not include_consumers:
         ts = TaskSystem([t for t in ts if not t.is_consumer])
-    min_period_task = min(ts, key=lambda t: t.period)
-    q_sigma = min_period_task.period
-    
-    consumer = SporadicTask(0, q_sigma, q_sigma)
-    consumer.is_consumer = True
-    consumer.preemption_level = -1
-    consumer.partition = 0
-    ts.append(consumer)
-    
     framework = get_framework('omnilog')
-    for t in ts:
-        t.cost = framework.calculate_execution_time(t)
-    sched = int(bound_response_times_omnilog(conf.num_cpus, ts, framework.delta, framework.beta))
-    return (sched, 0)
+    
+    for partition in iter_partitions_ts(ts):
+        part_ts = TaskSystem(partition)
+        if part_ts:
+            min_period_task = min(part_ts, key=lambda t: t.period)
+            q_sigma = min_period_task.period
+        else:
+            continue
+        consumer = SporadicTask(0, q_sigma, q_sigma)
+        consumer.is_consumer = True
+        consumer.preemption_level = -1
+        consumer.partition = part_ts[0].partition
+        part_ts.append(consumer)
+        for t in part_ts:
+            t.cost = framework.calculate_execution_time(t)
+        if not bound_response_times_omnilog(1, part_ts, framework.delta, framework.beta):
+            return (0, 0)
+    return (1, 0)
 
 def rta_nodrop_test(taskset, oh, conf, include_consumers=True):
-    ts = taskset
+    ts = TaskSystem(taskset)
     if not include_consumers:
         ts = TaskSystem([t for t in ts if not t.is_consumer])
     framework = get_framework('nodrop')
-    for t in ts:
-        t.cost = framework.calculate_execution_time(t)
-    sched = int(bound_response_times_nodrop(ts, framework.delta, framework.alpha, framework.beta))
-    return (sched, 0)
+
+    for partition in iter_partitions_ts(ts):
+        part_ts = TaskSystem(partition)
+        for t in part_ts:
+            t.cost = framework.calculate_execution_time(t)
+        if not bound_response_times_nodrop(part_ts, framework.delta, framework.alpha, framework.beta):
+            return (0, 0)
+        
+    return (1, 0)
 
 def setup_tests():
     return [
@@ -164,7 +225,7 @@ if __name__ == "__main__":
     class Conf:
         def __init__(self):
             self.experiment = 'util_num'
-            self.output = 'output/util_sched_rewrite_omnilog_n=1024.txt'
+            self.output = 'output/util_sched_rewrite_omnilog_consumer_partition_n=1.txt'
             self.num_task = 10
             self.samples = 5000
             self.periods = '10-100'
@@ -175,7 +236,7 @@ if __name__ == "__main__":
             # self.delta = 0.1
             # self.alpha = 0.2
             # self.beta = 0.05
-            self.consumer_period_factor = 1024.0
+            self.consumer_period_factor = 1.0
             self.consumer_syscall_count = 200
 
     conf = Conf()
